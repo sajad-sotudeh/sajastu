3da3c02485a3725fdf896038dfd41495321b5a05
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2015-08-02 04:59:02 -0700

    get_the_user_ip added

--- wp-content/plugins/fjn-poll/fjn-poll.php
+++ wp-content/plugins/fjn-poll/fjn-poll.php
@@ -232,4 +232,21 @@ function displayvotes(){
 					}
 				}
 }
+
+function get_the_user_ip(){
+
+    if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+    //check ip from share internet
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+    //to check ip is pass from proxy
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+
+} 
+
 ?>
